ce3ab00d6c233ad1c4f5f7737848e3fc3d9d40c4
prakhyat d rao <prakhyatdr@gmail.com>
2026-09-19 20:44:05 +0530

    Update server to listen on process.env.PORT
    Change server port to use environment variable.

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.set("layout", "./layouts/main");
 
 app.use("/", require("./server/routes/main.js"));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log("Server is running on port 3000");
 });
