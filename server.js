10734ca227cca398d2b3d60deb5f2fe2aecf8443
prakhyat d rao <prakhyatdr@gmail.com>
2026-09-21 20:58:18 +0530

    change port

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.set("layout", "./layouts/main");
 
 app.use("/", require("./server/routes/main.js"));
 
-app.listen(process.env.PORT, () => {
-    console.log("Server is running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is running on port");
 });
